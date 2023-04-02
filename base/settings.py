baff2804e4cd20c803b96669110d44cafd7f2431
joel ayertey <kvnqjoe@gmail.com>
2023-04-02 14:42:25 +0000

    setting up on render done

--- base/settings.py
+++ base/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
