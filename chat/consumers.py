07d3453bb644f95d5fa4af4b2bfcf23d02ee6116
joel ayertey <kvnqjoe@gmail.com>
2023-10-18 20:22:27 +0000

    mc

--- chat/consumers.py
+++ chat/consumers.py
@@ -12,7 +12,6 @@ from .serializers import GetMessageSerializer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["conversation_id"]
-        self.sender_id = self.scope["url_route"]["kwargs"]["sender_id"]
         self.room_group_name = f"chat{self.room_name}"
 
         # Join room group
@@ -42,21 +41,21 @@ class ChatConsumer(WebsocketConsumer):
     def chat_message(self, event):
         text_data_json = event.copy()
         text_data_json.pop("type")
-        message_text, attachment = (
+        message_text, sender_id = (
             text_data_json["message"],
-            text_data_json.get("attachment"),
+            text_data_json["sender"],
         )
 
         conversation = Conversation.objects.get(id=str(self.room_name))
-        sender = User.objects.get(id=self.sender_id)
+        sender = User.objects.get(id=sender_id)
 
         # to avoid duplicate messages
-        time_threshold = datetime.now() - timedelta(minutes=1)
-        similar_messages = Message.objects.filter(
-            text=message_text,
-            sender=sender,
-            created_at__gte=time_threshold,
-        )
+        # time_threshold = datetime.now() - timedelta(minutes=1)
+        # similar_messages = Message.objects.filter(
+        #     text=message_text,
+        #     sender=sender,
+        #     created_at__gte=time_threshold,
+        # )
 
         # if not similar_messages.exists():
         message = Message.objects.create(
